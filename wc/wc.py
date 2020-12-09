ebc324461490bd28ab7a853ed39355c6c4718ee2
tqtg <tuantq.vnu@gmail.com>
2020-12-09 21:10:44 +0800

    Update wordcloud

--- wc/wc.py
+++ wc/wc.py
@@ -10,6 +10,9 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 text = " ".join(l.strip() for l in open("abstracts.txt", "r").readlines()).lower()
 stopwords = set(STOPWORDS)
+stopwords.add("e")
+stopwords.add("g")
+stopwords.add("acm")
 
 wc = WordCloud(
     width=500,
